c7a2bbb35b1aa045bef97d36840ad56e5258ca59
davidhardin <thisgoestodavid@gmail.com>
2023-11-23 15:32:19 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import MovingPicture from './components/MovingPicture'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MovingPicture />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
